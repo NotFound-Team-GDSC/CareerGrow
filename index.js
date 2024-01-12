d5a6010044170d50849b4529fc45f1a95245d9ca
Roguemind <73373492+Roguemind@users.noreply.github.com>
2024-01-12 17:39:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import session from "express-session";
-// import db from './config/Database.js';
-// import SequelizeStore from "connect-session-sequelize"
 import route from './routes/route.js';
 dotenv.config();
 
 const app = express();
-
-// const sessionStore = SequelizeStore(session.Store);
-
-// const store = new sessionStore({
-//     db: db
-// })
-
-// app.use(session({
-//     secret: process.env.SESS_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     store: store,
-//     cookie: {
-//         secure: 'auto'
-//     }
-// }));
-
-// (async()=>{
-//     await db.sync();
-// })();  
-
 app.use(cors());
 app.use(express.json())
 app.use(route);
